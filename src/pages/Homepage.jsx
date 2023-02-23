4d56e3f855725b385a12ea2b54a00c49fd890cf7
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2023-02-23 22:43:29 +0530

    some stuff

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DateLoc } from '../components';
+import { NavLink } from 'react-router-dom';
 
 const Homepage = () => {
+	const [effect, setEffect] = useState(false);
 	return (
 		<>
 			{/* Hero section */}
-			<div className="grid px-12 pb-20 justify-start place-items-end min-h-screen text-slate-100 bg-gray-900 md:place-items-center md:pb-0">
+			<div className="grid px-12 pb-20 justify-start place-items-end min-h-screen text-slate-100 bg-slate-800 md:place-items-center md:pb-0">
 				<div>
-				<p className="sm:text-5xl text-7xl font-bold">CELESTIA</p>
-				<p className="py-6 text-3xl md:text-xl">Land of infinite possibilites.</p>
-				<DateLoc date={"today"} location={"Here"}/>
+					<p className="sm:text-5xl text-7xl font-bold">CELESTIA</p>
+					<p className="py-6 text-3xl md:text-xl">Land of infinite possibilites.</p>
+					<DateLoc date={"today"} location={"Here"}/>
 				</div>
 			</div>
 			{/* Throwback */}
-			<div className="flex gap-10 px-12 min-h-screen text-slate-100 bg-gray-900 justify-center items-center lg:flex-col">
-				<div className='flex-initial w-1/3 lg:w-auto'>
-				<p>What we're about?</p>
-				<p className="pt-12 text-xl font-bold">
-          Colosseum is the Annual Technical Festival of Don Bosco Institute of Technology hosted by the faculty and
-          various student committees of DBIT. These committees organize various event, as well as many other social
-          projects and outreach activities throughout a period of two days.<div className='py-2'/> Colosseum is well-known for holding a range
-          of activities such as contests, exhibits, talks, and seminars along with fun activities for students across
-          the campus!
-        </p>
+			<div className='px-12 md:px-5 min-h-screen text-slate-100 bg-slate-800 pt-20 flex flex-col justify-center'>
+				<div className="flex gap-10 justify-between items-start lg:flex-col-reverse">
+					<div className='flex-initial w-5/12 lg:w-auto'>
+						<p className='text-base md:text-sm'>What we're about?</p>
+						<div className="pt-12 text-xl font-bold lg:text-base md:font-semibold text-justify">
+							Colosseum is the Annual Technical Festival of Don Bosco Institute of Technology hosted by the faculty and
+							various student committees of DBIT. These committees organize various event, as well as many other social
+							projects and outreach activities throughout a period of two days.<div className='py-2'></div> Colosseum is well-known for holding a range
+							of activities such as contests, exhibits, talks, and seminars along with fun activities for students across
+							the campus!
+						</div>
+					</div>
+					<div className='w-[700px] md:w-11/12 aspect-video rounded-xl bg-gray-600 text-slate-50 bg-opacity-40 flex-grow' datatype='video'>
+						{/* Video */}
+					</div>
 				</div>
-				<div className='w-[700px] aspect-video rounded-xl bg-gray-600 text-slate-50 bg-opacity-40 flex-initial' datatype='video'>
-					{/* Video */}
+				<NavLink to="/">
+					<button className={`btn bg-gray-900 gap-2 ${effect && 'animate-buttonPop'}`} onClick={() => setEffect(true)} onAnimationEnd={() => setEffect(false)}>
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+						Button
+					</button>
+				</NavLink>
+			</div>
+			{/* Featuring */}
+			<div className='flex flex-col px-12 pb-20'>
+			<div className="grid gap-10 justify-start min-h-screen text-slate-100 bg-slate-800 md:place-items-center md:pb-0">
+				<div>
+					
 				</div>
+			</div>
 			</div>
 		</>
 	)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,19 @@ module.exports = {
       fontFamily: {
         'WorkSans': 'Work Sans',
       },
+      animation: {
+        buttonPop: 'buttonPop .25s ease-out',
+      },
+      transitionTimingFunction: {
+        'btn': 'cubic-bezier(.4,0,.2,1)',
+      },
+      keyframes: {
+        buttonPop: {
+          '0%': { transform: 'scale(1))' },
+          '40%': { transform: 'scale(0.95)' },
+          'to': { transform: 'scale(1)' },
+        },
+      },
     },
     screens: {
       'xl': {'max': '1200px'},
